c02b55dc80e9a4d29b643f9a17a591de17823654
Elchorco000 <ivandpp97@gmail.com>
2020-07-25 23:40:05 -0500

    Macros

---
+++ scriptRbtest.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+cd $ROOTBEER
+$ROOTSYS/bin/root -l -q .x $ROOTBEER/IVAN_ROOTBEER/Tesis/macro/Macro.C
+printf '//--------------------->\n//--------------------->\n//---------------------> \n'
+printf '%s' '//---------------------> Do you wish continue? [y,n]: '
+read ans
+if [[ "$ans" =~ "n" ]]; then
+    exit 1
+fi
+rehash
+cd ./IVAN_ROOTBEER/Tesis/Analysis/pos2neg1_skim/
+rbtest -Lfile.dat test.root
+$ROOTSYS/bin/root -l
+cd $ROOTBEER

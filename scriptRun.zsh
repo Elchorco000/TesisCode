6e1b66824e2e3b6a308aaa80e1ae18f6e2928b28
Ivan <ivan@localhost.localdomain>
2020-07-30 15:47:25 -0500

    Se arreglo algunas palabras

--- scriptRun.zsh
+++ scriptRun.zsh
@@ -7,15 +7,15 @@ FILEDATA=$(pwd)                       # #Define directory of DATA
 cd $ROOTBEER                          #
 #######################################
 
-$ROOTSYS/bin/root -l -q $FILE/Macro.C   #Run root and escecute Macro.C
+$ROOTSYS/bin/root -l -q $FILE/Macro.C   #Run root and execute Macro.C
 rehash
 printf '//--------------------->\n//--------------------->\n//---------------------> \n'
-printf '%s' '//---------------------> Do you wish continue? [y,n]: '
+printf '%s' '//---------------------> Do you want continue? [y,n]: '
 read ans
 if [[ "$ans" =~ "n" ]]; then
     exit 1
 fi                                
-cd $FILEDATA                            #Open File that has de data .root's
+cd $FILEDATA                            #Open File that has the data .root's
 pos2neg1 -Lfile.dat test.root           #Could you run one file .root or a file of list of a lot of .root's
 $ROOTSYS/bin/root -l                    #Run ROOT
 cd $FILE
